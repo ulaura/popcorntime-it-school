11f18d3a0bfbcfebb46f6372d881e1ff1c0c7db8
Laura Urian <lauraurdaswork@gmail.com>
2022-09-19 20:57:54 +0300

    Added favorites page ans styled movie favorites

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,22 +1,12 @@
-const API_URL = "https://api.themoviedb.org/3";
 const POSTER_PATH = "https://image.tmdb.org/t/p/original";
-const API_KEY = "35ab6d12331f50dd5c82138038e1b272";
 
-const searchElement = document.querySelector("#movieSearch");
 const moviesSection = document.querySelector("#movies");
 
 if(window.localStorage.getItem("movies") === null){
 window.localStorage.setItem("movies", JSON.stringify([]));
 }
 
-const favorites = JSON.parse(window.localStorage.getItem("movies"));
-
-const getData = async (query) => {
-  const movies = await fetch(
-    `${API_URL}/search/movie?api_key=${API_KEY}&query=${query}`
-  );
-  return movies.json();
-};
+let favorites = JSON.parse(window.localStorage.getItem("movies"));
 
 const createMovieElement = (movie) => {
   const divEl = document.createElement("div");
@@ -24,7 +14,7 @@ const createMovieElement = (movie) => {
   const description = document.createElement("p");
   const movieIdEl = document.createElement("span");
   const poster = document.createElement("img");
-  const addToFavorites = document.createElement("button");
+  const removeFromFavorites = document.createElement("button");
 
   poster.src = `${POSTER_PATH}/${movie.poster_path}`;
 
@@ -35,33 +25,29 @@ const createMovieElement = (movie) => {
 
   title.classList.add('text-4xl', 'font-bold', 'my-4');
   description.classList.add('text-sm');
-  addToFavorites.classList.add('bg-slate-500', 'rounded-lg', 'p-2', 'text-neutral-200');
+  removeFromFavorites.classList.add('bg-slate-500', 'rounded-lg', 'p-2', 'text-neutral-200');
   
   title.textContent = movie.title;
   description.textContent = movie.overview;
   movieIdEl.textContent = movie.id;
-  addToFavorites.textContent= "❤ Add to favorites";
+  removeFromFavorites.textContent= "💔 Remove from favorites";
 
-addToFavorites.addEventListener("click", ()=> {
-    favorites.push(movie);
-    window.localStorage.setItem("movies", JSON.stringify(favorites));
+removeFromFavorites.addEventListener("click", ()=> {
+    const newArray = favorites.filter(el => el !== movie);
+    favorites = [...newArray];
+    window.localStorage.setItem("movies", JSON.stringify(newArray));
+
+    moviesSection.innerHTML = "";
+
+    favorites.map(movie => createMovieElement(movie));
 })
   divEl.appendChild(poster);
   divEl.appendChild(title);
   divEl.appendChild(description);
   divEl.appendChild(movieIdEl);
-  divEl.appendChild(addToFavorites);
+  divEl.appendChild(removeFromFavorites);
 
   moviesSection.appendChild(divEl);
 };
 
-// favorites.map(movie => createMovieElement(movie));
-
-
-searchElement.addEventListener("change", async (e) => {
-  const value = e.target.value;
-  const { results } = await getData(value);
-
-  moviesSection.innerHTML = "";
-  results.map((item) => createMovieElement(item));
-});
+favorites.map(movie => createMovieElement(movie));
